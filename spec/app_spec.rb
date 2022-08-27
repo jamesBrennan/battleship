c26f6458902fe0ecfe8e369964ce3a5b18b35903
James Brennan <brennanmusic@gmail.com>
2022-08-27 17:29:35 +0300

    done

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,9 +3,11 @@ RSpec.describe App do
     App
   end
 
-  describe '.add' do
-    it 'sums the arguments' do
-      expect(app.add(2, 2)).to eq 4
+  describe '.max_subarray' do
+    it 'returns the value of the largest summed subarray' do
+      expect(App.max_subarray([-1, 1, 22, -33, 5])).to eq 23
+      expect(App.max_subarray([100, 50, 10, 20])).to eq 180
+      expect(App.max_subarray([-100, -10, -20, -50])).to eq -10
     end
   end
 end
--- src/app.rb
+++ src/app.rb
@@ -1,5 +1,15 @@
 class App
-  def self.add(a, b)
-    a + b
+  def self.max_subarray(input)
+    len = input.length - 1
+    value = -1_000_000_000
+
+    (0..len).each do |start_index|
+      (start_index..len).each do |end_index|
+        subarray = input[start_index..end_index]
+        value = [subarray.sum, value].max
+      end
+    end
+
+    value
   end
 end
